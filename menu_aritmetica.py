8abd8ba50f0ac352cd5882c704b50a9b4a5ae9de
FanorAndresAdarme-Martinez <norfa098@gmail.com>
2018-08-22 18:58:09 -0500

    creacion de opciones de mul y di

--- menu_aritmetica.py
+++ menu_aritmetica.py
@@ -1,9 +1,10 @@
 #declaracion de variables
 #creado por: Fanor Andres Adarme M
 #Fecha de creacion 22082018
-
 import os
 os.system("cls")
 print("MENU PRINCIPAL")
 print("1. Sumar numeros")
 print("2. Restar numeros")
+print("3. multiplicar numeros")
+print("4. dividir numeros")
