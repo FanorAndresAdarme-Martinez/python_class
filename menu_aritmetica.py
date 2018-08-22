ed9d1186940544c801cac2204924dd67c0093a05
FanorAndresAdarme-Martinez <norfa098@gmail.com>
2018-08-22 18:50:31 -0500

    ok

---
+++ menu_aritmetica.py
@@ -0,0 +1,9 @@
+#declaracion de variables
+#creado por: Fanor Andres Adarme M
+#Fecha de creacion 22082018
+
+import os
+os.system("cls")
+print("MENU PRINCIPAL")
+print("1. Sumar numeros")
+print("2. Restar numeros")
