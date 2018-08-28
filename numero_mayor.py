96130c2cff1694aef16df39008af729b05e519a8
FanorAndresAdarme-Martinez <norfa098@gmail.com>
2018-08-28 15:46:51 -0500

    codigo numero mayor

---
+++ numero_mayor.py
@@ -0,0 +1,11 @@
+#numero_mayor
+import os
+os.system("cls")
+n1 = 0
+n2 = 0
+n1 =int(input("ingrese primer numero: "))
+n2 =int(input("ingrese segundo numero: "))
+if n1 > n2:
+    print ("El numero mayor es: ",n1) 
+else :   
+    print ("El numero mayor es: ",n2)
