a20b6bd0f1da86eeab63d4f2391191fcadfd6b4a
FanorAndresAdarme-Martinez <norfa098@gmail.com>
2018-08-30 23:21:45 -0500

    ciclo 1

---
+++ ciclo1.py
@@ -0,0 +1,32 @@
+#OPERACION NUMEROS
+print ("1")
+print ("2")
+print ("3")
+print ("4")
+print ("5")
+print ("6")
+print ("7")
+print ("8")
+print ("9")
+print ("10")
+print ("***********************")
+#NUMEROS CONN CICLO WHILE
+num = 1
+while num <= 10:
+	print (num)
+	num = num +1
+print ("*******************")
+#TABLA DE MULTIPLICAR
+tabla = 5
+num = 1
+while num <= 10:
+	print (tabla, "*", num, "=", tabla * num)
+	num = num + 1
+print ("*******************")
+#INGRESAR LA TABLA USUARIO
+tabla = int(input("digite la tabla a multiplicar: "))
+num = 1
+while num <= 10:
+	print (tabla, "*", num, "=", tabla * num)
+	num = num + 1
+
