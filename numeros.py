77c8d1ae207f7a521691bae0dd37aebb66ef06b8
FanorAndresAdarme-Martinez <norfa098@gmail.com>
2018-09-05 10:32:39 -0500

    numeros +

---
+++ numeros.py
@@ -0,0 +1,10 @@
+#NUMEROS
+contador = 0
+
+i = 1
+print (i)
+while i <= 100 :
+    while rango <= 100 :
+        contador = contador + i
+    i = i + 1
+    print ("la suma de los numeros es: ",contador)
