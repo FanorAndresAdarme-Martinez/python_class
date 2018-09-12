91d29e93932b7179d1aaaa5c050bdab8dec185f4
FanorAndresAdarme-Martinez <norfa098@gmail.com>
2018-09-12 15:27:19 -0500

    primero

---
+++ average1_final.py
@@ -0,0 +1,13 @@
+#averrage1
+edad = 1 
+sumaed = 0
+personas = 0
+while edad <= 10 :
+	personas = int(input("ingrese edad: "))
+	sumaed = sumaed + personas
+	edad = edad + 1
+promedio = sumaed / 10
+print ("el promedio de las edades es: ",promedio)
+
+
+
