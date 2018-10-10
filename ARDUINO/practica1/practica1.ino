d942081b4bbb7685da909560f2230c47e222c350
FanorAndresAdarme-Martinez <norfa098@gmail.com>
2018-10-09 23:02:27 -0500

    Arduino

---
+++ ARDUINO/practica1/practica1.ino
@@ -0,0 +1,35 @@
+int LedRojo = 7;
+int LedVerde = 2;
+int LedAma = 4;
+int Motor = 12;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LedRojo,OUTPUT);
+  pinMode(LedVerde,OUTPUT);
+  pinMode(LedAma,OUTPUT);
+  pinMode(Motor,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LedRojo,HIGH);
+  delay(500); //Tiempo de espera
+  digitalWrite(LedRojo,LOW);
+  delay(500);
+  digitalWrite(LedAma,HIGH);
+  delay(500); //Tiempo de espera
+  digitalWrite(LedAma,LOW);
+  delay(500);
+  digitalWrite(LedVerde,HIGH);
+  delay(500); //Tiempo de espera
+  digitalWrite(LedVerde,LOW);
+  delay(500);
+  digitalWrite(LedAma,HIGH);
+  delay(500); //Tiempo de espera
+  digitalWrite(LedAma,LOW);
+  delay(500);
+   digitalWrite(Motor,HIGH);
+  delay(500); //Tiempo de espera
+  digitalWrite(Motor,LOW);
+  delay(500);  
+}
