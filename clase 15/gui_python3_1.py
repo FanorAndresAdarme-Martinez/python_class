89c7d11fc3c95a3043d50d2068f4ff6c8f17eb70
FanorAndresAdarme-Martinez <norfa098@gmail.com>
2018-12-03 13:26:28 -0600

    practica clase 15

--- clase 15/gui_python3_1.py
+++ clase 15/gui_python3_1.py
@@ -1,33 +1,41 @@
+#programa para control de arduino desde python
 import serial
 from tkinter import *
-
 import tkinter
 
-arduinoData = serial.Serial ('COM3',9600)
+valor = 0
+arduinoData = serial.Serial('COM9',9600)
 
-def onOff(valor):
-    arduinoData.write(valor)
+def led_onOff(valor):
+    arduinoData.write(b"valor")
+    print("encender todo")
+    
+def led_on_a():
+    arduinoData.write(b'6')
+    print("encender todo")
+
+def led_off_a():
+    arduinoData.write(b'7')
+    print("apagar todo")
 
 led_control_window = Tk()
-#Button = TKinter.Button
-btn1 = Button(led_control_window, text="ON", command=lambda:onOff(0))
-btn2 = Button(led_control_window, text="OFF", command=lambda:onOff(1))
-btn3 = Button(led_control_window, text="ON", command=lambda:onOff(2))
-btn4 = Button(led_control_window, text="OFF", command=lambda:onOff(3))
-btn4 = Button(led_control_window, text="ON", command=lambda:onOff(4))
-btn6 = Button(led_control_window, text="OFF", command=lambda:onOff(5))
-btn7 = Button(led_control_window, text="ON", command=lambda:onOff(6))
-btn8 = Button(led_control_window, text="OFF", command=lambda:onOff(7))
+#Button = Tkinter.button
+btn1 = Button(led_control_window, text="ON", command=lambda:led_onOff(0))
+btn2 = Button(led_control_window, text="OFF", command=lambda:led_onOff(1))
+btn3 = Button(led_control_window, text="ON", command=lambda:led_onOff(2))
+btn4 = Button(led_control_window, text="OFF", command=lambda:led_onOff(3))
+btn5 = Button(led_control_window, text="ON", command=lambda:led_onOff(4))
+btn6 = Button(led_control_window, text="OFF", command=lambda:led_onOff(5))
+btn7 = Button(led_control_window, text="ON  ALL", command=led_on_a)
+btn8 = Button(led_control_window, text="OFF ALL", command=led_off_a)
 
 btn1.grid(row=0,column=1)
-btn2.grid(row=1,column=1)
-btn1.grid(row=2,column=3)
-btn2.grid(row=3,column=3)
-btn1.grid(row=4,column=5)
-btn2.grid(row=5,column=5)
-btn1.grid(row=6,column=7)
-btn2.grid(row=7,column=7)
+btn2.grid(row=0,column=2)
+btn3.grid(row=1,column=1)
+btn4.grid(row=1,column=2)
+btn5.grid(row=2,column=1)
+btn6.grid(row=2,column=2)
+btn7.grid(row=3,column=1)
+btn8.grid(row=3,column=2)
 
 led_control_window.mainloop()
-
-    
--- clase 15/gui_python3_1/gui_python3_1.ino
+++ clase 15/gui_python3_1/gui_python3_1.ino
@@ -1,12 +1,13 @@
 char serialData;
-int pinRojo = 13;
-int pinVerde = 8;
-int pinAma = 10;
+int pinLed1 = 13;
+int pinLed2 = 10;
+int pinLed3 = 9;
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(pinRojo,OUTPUT);
-  pinMode(pinVerde,OUTPUT);
-  pinMode(pinAma,OUTPUT);
+  pinMode(pinLed1,OUTPUT);
+  pinMode(pinLed2,OUTPUT);
+  pinMode(pinLed3,OUTPUT);
   Serial.begin(9600);
 }
 
@@ -14,40 +15,22 @@ void loop() {
   // put your main code here, to run repeatedly:
   if(Serial.available()>0) {
     serialData = Serial.read();
-    Serial.println(serialData);
+    Serial.print(serialData);
 
-    if(serialData == '0'){
-      digitalWrite(pinRojo, HIGH);
-      }else{
-        if(serialData == '1'){
-          digitalWrite(pinRojo, LOW);
-           if(serialData == '2'){
-             digitalWrite(pinVerde, HIGH);
-             }else{
-               if(serialData == '3'){
-                 digitalWrite(pinVerde, LOW);
-                  if(serialData == '4'){
-                    digitalWrite(pinAma, HIGH);
-                    }else{
-                      if(serialData == '5'){
-                        digitalWrite(pinAma, LOW);
-                         if(serialData == '6'){
-                           digitalWrite(pinRojo,HIGH);
-                           digitalWrite(pinVerde,HIGH);
-                           digitalWrite(pinAma,HIGH);
-                           }else{
-                            if(serialData == '7'){
-                              digitalWrite(pinRojo,LOW);
-                              digitalWrite(pinVerde,LOW);
-                              digitalWrite(pinAma,LOW);
-                              }else{
-                              }
-                             }
-                            }
-                           }
-                          }
-                         }
-                        }
-                       }
-                      }
-                     }
+    if(serialData == '0') {
+      digitalWrite(pinLed1,HIGH);
+    }if(serialData=='1'){
+        digitalWrite(pinLed1,LOW);
+      }if(serialData=='2'){
+        digitalWrite(pinLed2,HIGH);
+        }if (serialData=='3'){
+          digitalWrite(pinLed2,LOW);
+          }if(serialData=='4'){
+            digitalWrite(pinLed3,HIGH);
+            }if (serialData=='5'){
+              digitalWrite(pinLed3,LOW);
+              }if(serialData=='6'){
+                digitalWrite(pinLed1,HIGH);}if (serialData=='3'){digitalWrite(pinLed2,LOW);
+              }
+  }
+}
